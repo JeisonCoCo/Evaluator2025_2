5c4c0422e0abd2e6576fc5782269925693769208
Jeison Cordoba <jeisonsmit@gmail.com>
2025-09-21 21:12:15 -0500

    create

---
+++ Evaluator/Evaluator.Cor/ExpressionEvaluator.cs
@@ -0,0 +1,11 @@
+﻿namespace Evaluator.Cor
+{
+    public class ExpressionEvaluator
+    {
+        public static double Evaluate(string infix)
+        {
+            
+            return 0;
+        }
+    }
+}
